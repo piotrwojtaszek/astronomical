b40388d2c8af82c05764674f65a559e2c9d59cda
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-10-26 11:57:48 +0100

    MovingPlanets

---
+++ Astronomical/Assets/Scripts/PlanetRotaror.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotaror : MonoBehaviour
+{
+    public Transform m_pivot;
+    public float m_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(m_pivot.position, Vector3.up, m_speed*Time.deltaTime);
+    }
+}
