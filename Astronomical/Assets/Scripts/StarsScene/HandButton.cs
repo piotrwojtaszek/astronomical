685c007f3948462d4c3c05dff73404e579b443b3
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-10-31 00:02:45 +0100

    Sockety

---
+++ Astronomical/Assets/Scripts/StarsScene/HandButton.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandButton : XRBaseInteractable
+{
+    public UnityEvent OnPress = null;
+    private float m_yMax = 0.0f;
+    private float m_yMin = 0.0f;
+    private bool m_previousPress = false;
+
+    private float m_previousHandHeight = 0.0f;
+    private XRBaseInteractor m_hoverInteractor = null;
+    protected override void Awake()
+    {
+        base.Awake();
+        onHoverEnter.AddListener(StartPress);
+        onHoverExit.AddListener(EndPress);
+    }
+
+    private void OnDestroy()
+    {
+        onHoverEnter.RemoveListener(StartPress);
+        onHoverExit.RemoveListener(EndPress);
+    }
+
+    private void StartPress(XRBaseInteractor interactor)
+    {
+        m_hoverInteractor = interactor;
+        m_previousHandHeight = GetLocalYPosition(m_hoverInteractor.transform.position);
+    }
+
+    private void EndPress(XRBaseInteractor interactor)
+    {
+        m_hoverInteractor = null;
+        m_previousHandHeight = 0.0f;
+
+        m_previousPress = false;
+        SetLocalYPosition(m_yMax);
+    }
+
+    private void Start()
+    {
+        SetMinMax();
+    }
+
+    private void SetMinMax()
+    {
+        Collider collider = GetComponent<Collider>();
+        m_yMin = transform.localPosition.y - (collider.bounds.size.y * 0.5f);
+        m_yMax = transform.localPosition.y;
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        if (m_hoverInteractor)
+        {
+            float newHandHeight = GetLocalYPosition(m_hoverInteractor.transform.position);
+            float handDiffrence = m_previousHandHeight - newHandHeight;
+            m_previousHandHeight = newHandHeight;
+
+            float newPosition = transform.localPosition.y - handDiffrence;
+            SetLocalYPosition(newPosition);
+
+            CheckPress();
+        }
+    }
+
+    private float GetLocalYPosition(Vector3 position)
+    {
+        Vector3 localPosition = transform.root.InverseTransformPoint(position);
+
+        return localPosition.y;
+    }
+
+    private void SetLocalYPosition(float position)
+    {
+        Vector3 newPosition = transform.localPosition;
+        newPosition.y = Mathf.Clamp(position, m_yMin, m_yMax);
+        transform.localPosition = newPosition;
+    }
+
+    private void CheckPress()
+    {
+        bool inPosition = InPosition();
+
+        if (inPosition && inPosition != m_previousPress)
+        {
+            OnPress?.Invoke();
+        }
+
+        m_previousPress = inPosition;
+    }
+
+    private bool InPosition()
+    {
+        float inRange = Mathf.Clamp(transform.localPosition.y, m_yMin, m_yMin + 0.01f);
+
+        return transform.localPosition.y == inRange;
+    }
+}
---
+++ Astronomical/Assets/Scripts/StarsScene/PrefabSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_spawnPoint;
+
+    [SerializeField]
+    private GameObject m_prefabs;
+    bool isRunning = false;
+
+    public void OnPress()
+    {
+        if (isRunning == false)
+            StartCoroutine(Spawn());
+    }
+
+
+    private IEnumerator Spawn()
+    {
+        isRunning = true;
+        Instantiate(m_prefabs, m_spawnPoint.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(.5f);
+        isRunning = false;
+    }
+}
