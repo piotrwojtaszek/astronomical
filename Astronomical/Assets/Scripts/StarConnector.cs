df5974b96fd0016bb970e9e5a8dbb58bd0768a59
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-11-07 17:32:45 +0100

    CustomSockets

---
+++ Astronomical/Assets/Scripts/StarConnector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StarConnector : MonoBehaviour
+{
+    public GameObject firstStar;
+    public GameObject secondStar;
+    LineRenderer lineRenderer;
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.SetPosition(0, firstStar.transform.position);
+
+        lineRenderer.SetPosition(1, secondStar.transform.position);
+
+    }
+}
---
+++ Astronomical/Assets/Scripts/StarsScene/StarSockets/StarDropZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarDropZone : MonoBehaviour
+{
+    public Transform interactableObject = null;
+    public Transform socetInteractor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactableObject != null)
+            UpdateSocetPosition();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (interactableObject == null)
+            interactableObject = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (interactableObject.GetInstanceID() == other.transform.GetInstanceID())
+            interactableObject = null;
+    }
+
+    void UpdateSocetPosition()
+    {
+        socetInteractor.position = interactableObject.position;
+    }
+}
