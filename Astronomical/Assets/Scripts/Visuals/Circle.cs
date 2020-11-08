bd81373eaf1d696558be3386039afd9845c72f16
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-11-08 22:35:35 +0100

    SolarSystem

---
+++ Astronomical/Assets/Scripts/Visuals/Circle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Circle : MonoBehaviour
+{
+    public int segments;
+    public float xradius;
+    public float yradius;
+    LineRenderer line;
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.positionCount = segments + 1;
+        line.useWorldSpace = false;
+        CreatePoints();
+    }
+
+
+    void CreatePoints()
+    {
+        float x;
+        float z;
+        float y = 0f;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+            z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+
+            line.SetPosition(i, new Vector3(x, y, z));
+
+            angle += (360f / segments);
+        }
+    }
+}
