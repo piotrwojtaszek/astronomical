b740a63818746fbad2e830c79b88ffbee2aba145
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-11-11 17:34:08 +0100

    Improvment Socket

---
+++ Astronomical/Assets/Scripts/Interactions/ObjectsInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsInfo : MonoBehaviour
+{
+    public bool picked = false;
+
+    public void SelectEnter()
+    {
+        picked = true;
+    }
+
+    public void SelectExit()
+    {
+        picked = false;
+    }
+}
---
+++ Astronomical/Assets/Scripts/Interactions/SocketInteractions/StarSockets/CustomSocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomSocket : MonoBehaviour
+{
+    ObjectsInfo interactable = null;
+    [SerializeField]
+    Transform socket;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactable && !interactable.picked)
+        {
+            socket.position = interactable.transform.position;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Interactable") && other.GetComponent<ObjectsInfo>())
+        {
+            interactable = other.GetComponent<ObjectsInfo>();
+            socket.position = interactable.transform.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            interactable = null;
+        }
+    }
+}
