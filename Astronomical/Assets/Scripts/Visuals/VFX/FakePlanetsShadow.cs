d849cc2c53c5518d8960494ef0693f0115db0752
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-11-22 18:01:09 +0100

    SolarEclipse

--- Astronomical/Assets/Scripts/Visuals/VFX/FakePlanetsShadow.cs
+++ Astronomical/Assets/Scripts/Visuals/VFX/FakePlanetsShadow.cs
@@ -6,11 +6,14 @@ public class FakePlanetsShadow : MonoBehaviour
 {
     [SerializeField]
     Transform parent = null;
-    [SerializeField]
-
+    public Vector3 offset;
     // Update is called once per frame
+    private void Start()
+    {
+        offset = transform.position - parent.position;
+    }
     void Update()
     {
-        transform.position = parent.position;
+        transform.position = parent.position + offset;
     }
 }
