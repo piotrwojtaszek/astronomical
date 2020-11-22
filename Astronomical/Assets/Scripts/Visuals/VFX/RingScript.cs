243a04e923300c2ba2b6c25c192f670c59ac68d6
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-11-22 19:00:55 +0100

    RingScriptError

---
+++ Astronomical/Assets/Scripts/Visuals/VFX/RingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingScript : MonoBehaviour
+{
+    public GameObject prefab;
+    public int segments;
+    public float xradius;
+    public float yradius;
+    void Start()
+    {
+        CreatePoints();
+    }
+
+
+    void CreatePoints()
+    {
+        float x;
+        float z;
+        float y = 0f;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+            z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+            GameObject obj = Instantiate(prefab);
+            Vector3 pos = transform.position;
+            obj.transform.position = pos + new Vector3(x, y, z);
+            angle += (360f / segments);
+        }
+    }
+
+}
