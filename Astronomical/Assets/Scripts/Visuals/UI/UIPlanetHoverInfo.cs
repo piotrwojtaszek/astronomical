0a00f78ebf5f5f75cda3dd56e3d62d968640f2a0
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-12-01 22:55:41 +0100

    HoverInfoTest

---
+++ Astronomical/Assets/Scripts/Visuals/UI/UIPlanetHoverInfo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UIPlanetHoverInfo : MonoBehaviour
+{
+    public GameObject canvas;
+    public void Select()
+    {
+        canvas.SetActive(true);
+    }
+    public void UnSelect()
+    {
+        canvas.SetActive(false);
+    }
+}
